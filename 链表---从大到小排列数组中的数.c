d6b9007db1a24ab53a9bf58ff7cbc2d37fa9265f
william-s-alt <124338668+william-s-alt@users.noreply.github.com>
2023-02-03 15:30:30 +0800

    Add files via upload

---
+++ 链表---从大到小排列数组中的数.c
@@ -0,0 +1,80 @@
+﻿
+
+#include <stdio.h>
+
+#include<malloc.h>
+
+struct Link
+
+{
+    int data;
+
+    struct Link* next;
+
+};
+
+void InsertList(struct Link* H, int n)
+
+{
+    struct Link* p, * q, * s;
+
+    s = (struct Link*)malloc(sizeof(struct Link));//seprate 中间转换值
+
+    s->data = n;
+
+    q = H; p = H->next;
+
+    while (p && n > p->data)
+    { 
+        q = p; 
+        p = p->next;
+    }
+    
+    s->next = q->next;
+    q->next = s;//插入
+
+    
+
+}
+
+void main()
+
+{
+    int a[] = { 12,3,45,67,7,65,10,20,35,55 }; int i;
+
+    struct Link* H, * p;
+
+    H = (struct Link*)malloc(sizeof(struct Link));
+
+    H->next = NULL;
+
+    for (i = 0; i < 10; i++) InsertList(H, a[i]);
+
+    p = H->next;
+
+    while (p)
+
+    {
+
+        printf(" % 4d", p->data);
+
+        p = p->next;
+
+    }
+
+    printf("\n");
+
+}
+//#include <stdio.h>
+//#include<stdlib.h>
+//#include <string.h>
+//#include <errno.h>
+//void main()
+//
+//{
+//	
+//
+//
+//	return 0;
+//}
+////}
